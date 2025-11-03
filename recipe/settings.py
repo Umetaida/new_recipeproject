26356b31f72a6a7a7805607bf1eba65d0f5431fa
umezawa aki <umezawaaki@umezawanoMacBook-Air.local>
2025-11-03 14:55:35 +0900

    3

--- recipe/settings.py
+++ recipe/settings.py
@@ -11,11 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import dj_database_url
-from pathlib import Path
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
